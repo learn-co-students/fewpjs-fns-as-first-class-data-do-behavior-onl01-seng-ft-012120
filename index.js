372f7b0063839a573b20aeb235326dcf79169052
cosmicChimp <ian.t.marrs@gmail.com>
2020-04-23 11:51:46 -0600

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 /* Given Code, don't edit */
 
-document.querySelector("button").addEventListener("click", handleClick);
+document.querySelectorAll("input")[1].addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
+  
   displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(s) {
+  let s_array = s.split(":")
+  let int = parseInt(s_array[0])
+  console.log(int);
+  if (int < 12) {
+    return "Good Morning"
+  } else if (int >= 12 && int < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
 
-/* Write your implementation of greet() */
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  let element = document.querySelector("#greeting")
+  element.innerText = str
+}
